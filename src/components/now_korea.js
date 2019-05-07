475692290bf32559aa23392eb10e94a282b9f1ed
S00ahKim <rlatndk0206@naver.com>
2019-05-07 15:18:57 +0900

    update-remove

--- src/components/now_korea.js
+++ src/components/now_korea.js
@@ -35,17 +35,6 @@ class NowKorea extends Component {
     }
 
     componentWillUpdate(nextState){ 
-        var self = this;
-        axios.get('https://api.waqi.info/feed/seoul/?token=51c05eb19f4ac4b5cbb7e2b56a73d8526f944e82')
-            .then(response => self.setState({seD: [response.data.data.aqi]}));
-        axios.get('https://api.waqi.info/feed/busan/?token=51c05eb19f4ac4b5cbb7e2b56a73d8526f944e82')
-            .then(response => self.setState({buD: [response.data.data.aqi]}));
-        axios.get('https://api.waqi.info/feed/daegu/?token=51c05eb19f4ac4b5cbb7e2b56a73d8526f944e82')
-            .then(response => self.setState({dgD: [response.data.data.aqi]}));
-        axios.get('https://api.waqi.info/feed/incheon/?token=51c05eb19f4ac4b5cbb7e2b56a73d8526f944e82')
-            .then(response => self.setState({inD: [response.data.data.aqi]}));
-        axios.get('https://api.waqi.info/feed/gwangju/?token=51c05eb19f4ac4b5cbb7e2b56a73d8526f944e82')
-            .then(response => self.setState({gwD: [response.data.data.aqi]}));
     }
     
     componentDidUpdate(prevProps, prevState, snapshot) { 
